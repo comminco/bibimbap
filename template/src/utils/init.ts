970e3630c03a1d7adf51d3450cbc64b803f40891
comminco <iam@commin.co>
2023-01-28 12:48:04 +0900

    fix:update init function

--- template/src/utils/init.ts
+++ template/src/utils/init.ts
@@ -1,9 +1,31 @@
 /**
  * once execute
  */
-export function Init() {
+export function init() {
+  easterEgg()
+  opening()
+}
+
+/**
+ * after rendering, remove intro and appear components
+ */
+const opening = () => {
+  setTimeout(() => {
+    const introEl = document.getElementById('intro')!
+    const bibimbapEl = document.getElementById('_bibimbap')!
+
+    bibimbapEl.style.animation = 'appear linear 1.5s 1 Forwards'
+    introEl.style.animation = 'disappear linear 1s 1 Forwards'
+
+    setTimeout(() => {
+      introEl.remove()
+    }, 1200)
+  }, 2200)
+}
+
+const easterEgg = () => {
   console.log(
-    '%c Thank you for commin! - https://commin.co',
-    'background: #337dae; color: #fff; font-size: 12px; font-weight:bold'
+    '%c by commin - https://commin.co',
+    'color: #4A6F84; font-size: 12px; font-weight:bold; a { color: #fff;}'
   )
 }
