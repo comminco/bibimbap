e64ed47e87ed8e08a4a5a2a5911bd1df93aa28fd
comminco <iam@commin.co>
2023-02-11 16:08:39 +0900

    feat: language recoil

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -4,11 +4,16 @@ import { Routes, Route } from 'react-router-dom'
 import '@/css/globalStyles.css'
 import Home from '@/views/home'
 import { init } from '@/utils/init'
+import { timeZone as timeZoneRecoil } from '@/atoms/user'
+import { useSetRecoilState } from 'recoil'
 
 function App() {
+  const setTimeZone = useSetRecoilState(timeZoneRecoil)
+
   useEffect(() => {
       init()
   }, [])
+  
   return (
     <Routes>
       <Route path="/" element={<Home />} />
--- template/src/atoms/user.ts
+++ template/src/atoms/user.ts
@@ -1,4 +1,6 @@
 import { atom } from 'recoil'
+
+
 export const timeZone = atom<string>({
   key: 'timeZone',
   default: 'Asia/Seoul',
